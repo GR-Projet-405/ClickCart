e3ef983468317af53ced5fab3152cd9601d110f0
Ashen Lakmal <kahadawaarachchiashen@gmail.com>
2026-09-25 12:43:03 +0530

    Revert "Feature/dev 07 service pricing packages"

--- backend/src/main/java/com/clickcart/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.clickcart.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
